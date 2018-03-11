eb34c0686ae211ce7eeba86f6ed58aa65f8409eb
Hamza Anis <hamzaanis9514@gmail.com>
2018-03-12 00:41:30 +0500

    Init commit 🎉

---
+++ app/src/main/java/io/github/hamzaanis/cryptocalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.hamzaanis.cryptocalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
